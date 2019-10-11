f815ac47cc7737825af2c0643e6bc5d2a6d4359b
jecos <jecos.crsj@gmail.com>
2019-10-11 17:22:27 -0400

    🔧 Download jwt public key only once

--- app/services/AuthService.scala
+++ app/services/AuthService.scala
@@ -5,19 +5,21 @@ import java.time.Clock
 import javax.inject.Inject
 import pdi.jwt.{JwtAlgorithm, JwtClaim, JwtJson}
 import play.api.Configuration
+import utils.Control.using
 
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 class AuthService @Inject()(config: Configuration) {
-  import Control._
 
   implicit val clock: Clock = Clock.systemUTC
 
+  private val key = using(Source.fromURL(config.get[String]("jwt.public_key.url"))) { source => source.mkString }
+
   def validateJwt(token: String): Try[JwtClaim] = for {
-      claims <- JwtJson.decode(token, key(config.get[String]("jwt.public_key.url")), Seq(JwtAlgorithm.RS256))
-      _ <- validateClaims(claims)
-    } yield claims
+    claims <- JwtJson.decode(token, key, Seq(JwtAlgorithm.RS256))
+    _ <- validateClaims(claims)
+  } yield claims
 
   private val validateClaims = (claims: JwtClaim) =>
     if (claims.isValid(clock)) {
@@ -26,14 +28,6 @@ class AuthService @Inject()(config: Configuration) {
       Failure(new Exception("The JWT did not pass validation"))
     }
 
-  private val key = (path: String) => using(Source.fromURL(path)) { source => source.mkString}
 }
 
-object Control {
-  def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B =
-    try {
-      f(resource)
-    } finally {
-      resource.close()
-    }
-}
+
